71c75649560cfdf0bfa78fa94afbe46e444bf9a8
ningwei <wei.ning@guuidea.com>
2019-05-16 11:30:13 +0800

    修改备份方法

--- .vimrc
+++ .vimrc
@@ -1,11 +1,15 @@
 
 
 
+" show line number
 set number
+
 set tabstop=4
 set softtabstop=4
 set shiftwidth=4
 set expandtab
+set autoindent
+
 inoremap jk <esc>
 inoremap ' ''<esc>i
 inoremap ( ()<esc>i
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # 检测.vimrc是否已经存在。如果存在，则移入备份文件夹中
 if [ -f "~/.vimrc" ]; then
-    mkdir "~/vimrc-backup" && mv ~/.vimrc ~/vim-backup
+    mv ~/.vimrc ~/.vimrc-backup
     if [ $? -ne 0 ]; then
         echo "backup fail"
         exit 0
